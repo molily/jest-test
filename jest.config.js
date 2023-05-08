ff41d45dfc509684b0dcf176d283b7e6ff7b13e4
Mathias Schaefer (molily) <molily@mailbox.org>
2023-05-08 23:52:53 +0200

    Remove testing library dom

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,9 @@
 /** @type {import('@jest/types').Config.InitialOptions} */
 const config = {
-  // setupFilesAfterEnv: ["<rootDir>/setupTestFramework.js"],
   transform: {},
   testEnvironment: "jsdom",
   clearMocks: true,
   restoreMocks: true,
 };
 
-// eslint-disable-next-line import/no-default-export
 export default config;
